8cff34cbbb6366bfec7722a369748c755a46bcd9
Siwawes Wongcharoen <siwaoh@gmail.com>
2016-06-28 17:22:50 +0700

    update todo

---
+++ lab_post.php
@@ -0,0 +1,12 @@
+<?php
+// TODO: Test post data
+
+// TODO: create database
+// TODO: create table people(id, name)
+
+// TODO: connect to MySQL
+
+// TODO: insert data to database
+
+// TODO: close connection
+?>
